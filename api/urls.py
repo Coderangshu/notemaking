35a5d49c65682290cf3128f0bae1b388aa2314d0
Prasoon <prasoonkumar054@gmail.com>
2024-11-10 19:29:35 +0530

    Backend part almost done

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
     path('notes/', views.getNotes, name='notes'),
-    path('note/<str:pk>/', views.getNote, name='note'),
+    path('notes/<str:pk>/', views.getNote, name='note'),
+    path('notes/<str:pk>/share/', views.shareNote, name='share-note'),
+    path('auth/signup/', views.signup, name='signup'),
+    path('auth/login/', views.login, name='login'),
+    path('auth/logout/', views.logout, name='logout'),
 ]
